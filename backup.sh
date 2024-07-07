5767da1db354da39b6e56c1f0f5c994d1d66467d
Chikobara <albaraa.as.2002@gmail.com>
2024-07-07 09:41:36 +0300

    Update

--- backup.sh
+++ backup.sh
@@ -14,33 +14,57 @@ copy_item() {
 }
 
 
+
 # Backup Hyprland config
+echo
 echo "Backup Hyprland config"
-copy_item "$HOME/.config/hypr" "$script_dir/"
+copy_item "$HOME/.config/hypr" "$script_dir/.config/"
+
+# Backup wlogout config
+echo
+echo "Backup wlogout config"
+copy_item "$HOME/.config/wlogout" "$script_dir/.config/"
+
+# Backup fuzzel config
+echo
+echo "Backup fuzzel config"
+copy_item "$HOME/.config/fuzzel" "$script_dir/.config/"
+
+
+# Backup qt config
+echo
+echo "Backup qt5ct & qt6ct config"
+copy_item "$HOME/.config/qt5ct" "$script_dir/.config/"
+copy_item "$HOME/.config/qt6ct" "$script_dir/.config/"
+
+
 
 # Backup AGS config
+echo
 echo "Backup AGS config"
-copy_item "$HOME/.config/ags" "$script_dir/"
+copy_item "$HOME/.config/ags" "$script_dir/.config/"
 
-# Backup themes and icons
-echo "Backup themes and icons"
-copy_item "$HOME/.themes" "$script_dir/"
-copy_item "$HOME/.icons" "$script_dir/"
 
 # Backup Kitty config
+echo
 echo "Backup Kitty config"
-copy_item "$HOME/.config/kitty" "$script_dir/"
+copy_item "$HOME/.config/kitty" "$script_dir/.config/"
 
 # Backup zsh configs
+echo
 echo "Backup zsh configs"
 copy_item "$HOME/.zshrc" "$script_dir/"
-copy_item "$HOME/.zshenv" "$script_dir/"
 copy_item "$HOME/.zprofile" "$script_dir/"
+copy_item "$HOME/.zshenv" "$script_dir/"
 copy_item "$HOME/.zsh" "$script_dir/"
+copy_item "$HOME/.config/zshrc.d" "$script_dir/.config/"
 
 # Backup fonts
+echo
 echo "Backup fonts"
-copy_item "$HOME/.local/share/fonts" "$script_dir/"
+copy_item "$HOME/.local/share/fonts" "$script_dir/.local/share/"
 copy_item "$HOME/.fonts" "$script_dir/"
 
+
+echo
 echo "Backup completed in: $script_dir"
