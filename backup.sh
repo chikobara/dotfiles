56e0026f05cc77a9cc561535d77f1cf0abd63af7
Chikobara <albaraa.as.2002@gmail.com>
2024-07-07 09:03:01 +0300

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	modified:   backup.sh

--- backup.sh
+++ backup.sh
@@ -2,6 +2,8 @@
 
 script_dir="$(pwd)"
 # Function to copy files/directories, replacing if they exist
+
+
 copy_item() {
     if [ -e "$1" ]; then
         rsync -a --delete "$1" "$2"
@@ -13,25 +15,31 @@ copy_item() {
 
 
 # Backup Hyprland config
+echo "Backup Hyprland config"
 copy_item "$HOME/.config/hypr" "$script_dir/"
 
 # Backup AGS config
+echo "Backup AGS config"
 copy_item "$HOME/.config/ags" "$script_dir/"
 
 # Backup themes and icons
+echo "Backup themes and icons"
 copy_item "$HOME/.themes" "$script_dir/"
 copy_item "$HOME/.icons" "$script_dir/"
 
 # Backup Kitty config
+echo "Backup Kitty config"
 copy_item "$HOME/.config/kitty" "$script_dir/"
 
 # Backup zsh configs
+echo "Backup zsh configs"
 copy_item "$HOME/.zshrc" "$script_dir/"
 copy_item "$HOME/.zshenv" "$script_dir/"
 copy_item "$HOME/.zprofile" "$script_dir/"
 copy_item "$HOME/.zsh" "$script_dir/"
 
 # Backup fonts
+echo "Backup fonts"
 copy_item "$HOME/.local/share/fonts" "$script_dir/"
 copy_item "$HOME/.fonts" "$script_dir/"
 
